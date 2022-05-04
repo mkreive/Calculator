da689e113aa8aa3cf4a1402b9f781ba8d589c623
mkreive <mkreive@gmail.com>
2022-05-04 12:50:58 +0300

    code: theme switching done

--- script.js
+++ script.js
@@ -3,9 +3,28 @@
 // DATA
 
 // SELECTORS
-
+const app = document.getElementById("app-root");
+const themeSwithBtns = document.querySelectorAll(".toggle__buttons-btn");
+const themeCircle = document.querySelector(".toggle__bar");
 // VARIABLES
 
 // HELPER FUNCTIONS
 
 // LISTENERS
+themeSwithBtns.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+        const theme = e.target.innerText;
+        app.classList.remove(app.classList.value);
+        app.classList.add(`theme-${theme}`);
+
+        if (theme == 1) {
+            themeCircle.style.justifyContent = "flex-start";
+        } else if (theme == 2) {
+            themeCircle.style.justifyContent = "center";
+        } else if (theme == 3) {
+            themeCircle.style.justifyContent = "flex-end";
+        } else {
+            console.error("Error changing themes");
+        }
+    });
+});
